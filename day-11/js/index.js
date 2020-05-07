1d936a317a6d40d0ef5515e7232bcdf6993daa6d
Rebecca Klein <klein324@uw.edu>
2020-05-07 15:25:17 -0700

    day 11 + 12

--- day-11/js/index.js
+++ day-11/js/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Base of the api
-const urlBase = "https://api.github.com/search/repositories?sort=stars&q=";
+const URLBASE = "https://api.github.com/search/repositories?sort=stars&q=";
 
 // Create a click event for your button
 $('form').submit(function (event) {
@@ -9,12 +9,20 @@ $('form').submit(function (event) {
     event.preventDefault();
 
     // Get the value of your queryInput, and construct your API query
-
+    let query = $('#queryInput').val();
+    let url = URLBASE + query;
     // Fetch the data at that URL, THEN
+    fetch(url)
     // Return the `.json()` of the response, THEN
+        .then(function(response) {
+            return response.json();
+        })
     // Pass the results to renderSearchResults, make sure to CATCH
+        .then(renderSearchResults)
     // Any of your errors
-
+        .catch(function(error) {
+            console.error(error);
+        });
 
     return false; // prevent unwanted page behavior
 });
@@ -23,14 +31,22 @@ $('form').submit(function (event) {
 // You should display the title as a link, and
 // The description of the repo
 function renderItem(item, parent) {
-
+    let ele = $('<li>');
+    parent.append(ele);
+    ele.append(`<string><a href="${item.html_url}">${item.full_name}</a></strong>`)
+    ele.append(`<span>: ${item.description}</span>`)
 }
 
 // Write a function to render search results. 
 // It should iterate through items and call the renderItem method
 function renderSearchResults(results) {
+    // Empty out content div
+    $('#content').empty();
     // Create a new ul as the parent + append it to the body
-
+    let ul = $('<ul>');
+    $('#content').append(ul);
     // Iterate through results and call the renderItem method
-
+    results.items.map(function(d) {
+        renderItem(d, ul);
+    });
 }
--- day-12/js/index.js
+++ day-12/js/index.js
@@ -4,10 +4,28 @@
 // Class Puppy
 class Puppy {
     // Constructor: takes in an img url, and a sound
+    constructor(
+        img = "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12231413/Labrador-Retriever-MP.jpg",
+        sound = "WOOF!"
+               ) {
+        this.img = img;
+        this.sound = sound;
+    }
 
     // Method for "speaking" using responsiveVoice
+    bark() {
+        responsiveVoice.speak(this.sound)
+    }
 
     // Render a Div that you can click on to bark
+    render() {
+        let puppyCard = $('<div>');
+        puppyCard.attr('title', `puppy that says ${this.sound}`);
+        puppyCard.attr('class', 'puppyCard col-sm-4');
+        puppyCard.css('background-image', `url(${this.img})`);
+        puppyCard.click(() => this.bark());
+        return puppyCard;
+    }
 
 }
 
@@ -52,23 +70,49 @@ class PuppyForm {
 // Class for the app
 class PuppyApp {
     // Constructor: takes in a parent element and list of puppies
+    constructor(parentElement, puppyList) {
+        this.parentElement = parentElement;
+        this.puppyList = puppyList;
+    }
 
     // Add puppy: pushes new data into list of puppies and re-renders the app
-
+    addPuppy(img, sound) {
+        console.log(this);
+        this.puppyList.push({
+            url: img,
+            sound: sound
+        });
+        this.render();
+    }
 
     // Render;
     render() {
         // Empty parent element
-
+        this.parentElement.empty();
 
         // Create and render a new form
+        let form = new PuppyForm(this.addPuppy.bind(this));
+        this.parentElement.append(form.render());
 
         // Append puppy list element to parent (in a wrapper div)
+        let puppyWrapper = $('<div class="row">');
+        this.parentElement.append(puppyWrapper)
+        this.puppyList.map((puppyInfo) =>{
+            let newPuppy = new Puppy(puppyInfo.url, puppyInfo.sound);
+            puppyWrapper.append(newPuppy.render());
+        })
 
     }
 }
 
 // Create a new app with a single puppy
-
+let app = new PuppyApp(
+    $('#content'),
+    [{
+        url: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12231413/Labrador-Retriever-MP.jpg",
+        sound: "bark"
+    }]
+);
 
 // Render the app
+app.render();
